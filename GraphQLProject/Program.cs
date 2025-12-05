b3deff2bcad9cf5bef74ac5a6bdd76b8733a2db4
rakdav <rakdav@inbox.ru>
2025-12-05 13:15:26 +0200

    last

--- GraphQLProject/Program.cs
+++ GraphQLProject/Program.cs
@@ -2,6 +2,7 @@ using GraphQLProject.DataAccess.DAO;
 using GraphQLProject.DataAccess.Data;
 using GraphQLProject.DataAccess.Entity;
 using HotChocolate.Execution;
+using HotChocolate.Subscriptions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,7 @@ builder.Services.AddDbContext<SampleAppDbContext>(options=>options.UseSqlServer(
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddGraphQLServer().AddQueryType<Query>().
-    AddMutationType<Mutation>().AddSubscriptionType<Subscription>();
+    AddMutationType<Mutation>().AddSubscriptionType<Subscription>().AddInMemorySubscriptions();
 builder.Services.AddScoped<DepartmentRepository, DepartmentRepository>();
 builder.Services.AddScoped<EmployeeRepository, EmployeeRepository>();
 var app = builder.Build();

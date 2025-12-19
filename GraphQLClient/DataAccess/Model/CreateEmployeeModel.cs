d959db4c347309c6819f42ca7e702c721aba8f89
rakdav <rakdav@inbox.ru>
2025-12-19 13:27:30 +0200

    1

---
+++ GraphQLClient/DataAccess/Model/CreateEmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GraphQLClient.DataAccess.Model
+{
+    public class CreateEmployeeModel
+    {
+        [Required(ErrorMessage="Name is required")]
+        public string? Name {  get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Age Name is required")]
+        [Range(minimum:20,maximum:50)]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Department Name is required")]
+        public string? DepartmentName { get; set; }
+
+
+    }
+}
---
+++ GraphQLClient/DataAccess/Model/CreateEmployeeReturnModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQLClient.DataAccess.Model
+{
+    public class CreateEmployeeReturnModel
+    {
+        public int DepartmentId {  get; set; }
+        public string? Name { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
